8185c918be95ef404a12b30fb40fa8ce0bf5bf0c
lingquan <lingquanhan@gmail.com>
2019-04-14 06:52:35 -0700

    all files

--- main.py
+++ main.py
@@ -1,35 +1,44 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START gae_python37_app]
-from flask import Flask
-
-
+from flask import Flask, render_template,request, jsonify
+import threading
+import speech
+import time
+import json
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = Flask(__name__)
 
+data = {}
+
 
 @app.route('/')
 def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    print("run runrun")
+    return render_template('index.html')
+
+
+@app.route('/background_process_test', methods=['POST'])
+def background_process_test():
+    print("Hello！！！！！！！！！")
+    global data
+    data = speech.main()
+    print(data)
+
+
+@app.route('/getdata')
+def get_data():
+    print("get data！！！！！！！！！")
+    global data
+    print("cur data", data)
+    # data = {'name': "John", 'age': 31, 'city': "New York"}
+    # res = data['score']
+    # print("res", res)
+    return jsonify(data), 201
 
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
+
     app.run(host='127.0.0.1', port=8080, debug=True)
 # [END gae_python37_app]
